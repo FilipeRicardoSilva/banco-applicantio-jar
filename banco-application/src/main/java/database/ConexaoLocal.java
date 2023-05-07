c97387f138c6bfd2e60a90e5b05a5ded24a89d56
FilipeRicardoSilva <filipe.silva@sptech.school>
2023-05-06 23:02:45 -0300

    Dockerfile

--- banco-application/src/main/java/database/ConexaoLocal.java
+++ banco-application/src/main/java/database/ConexaoLocal.java
@@ -10,25 +10,32 @@ package database;
  */
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+    
 public class ConexaoLocal {
 
     private JdbcTemplate conexaoDoBanco;
 
     public ConexaoLocal() {
-//172-31-28-45
- 
+
+      
+
+
 
         /* MYSLQ WORKBENCH = LOCAL */
+        
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         // PROCURAR CLASS NAME DO MYSQL WORKBENTH OU MYSQL
-        dataSource.setUrl("//172.31.28.45:3307/renderWatch?autoReconnect=true&useSSL=false");
+        dataSource.setUrl("jdbc:mysql://mydatabase:3307/renderWatch");
         //COLOCAR URL DA AZURE OU BANCO LOCAL / para conectar em banco local, utilizar localhost e porta
-        dataSource.setUsername("ContainerSQL");
+        dataSource.setUsername("root");
         dataSource.setPassword("#Gfgrupo6");
 
         this.conexaoDoBanco = new JdbcTemplate(dataSource);
+       
 
       
     }
