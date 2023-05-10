186dcdb981d1d9c3639c6e1caca848fd1b453a85
FilipeRicardoSilva <filipe.silva@sptech.school>
2023-05-10 20:13:24 -0300

    jar

--- banco-application/src/main/java/database/ConexaoLocal.java
+++ banco-application/src/main/java/database/ConexaoLocal.java
@@ -30,7 +30,7 @@ public class ConexaoLocal {
 
         /* SLQ SERVER NUVEM = AZURE */
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql:/localhost/renderwatch");
+        dataSource.setUrl("jdbc:mysql:/localhost:8080/renderwatch");
         dataSource.setUsername("root");
         dataSource.setPassword("urubu100");
         
